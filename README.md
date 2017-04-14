c8f8681c58b1d3f8c4c7227d0a9db6f9ead9bf8e
Evheniy Bystrov <evheniy.bystrov@gmail.com>
2017-04-14 16:56:11 -0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,126 @@
-# yeps-virtual-host
-yeps-virtual-host
+# YEPS Virtual Host
+
+It helps to work with different domains splitting http / https protocols
+
+[![NPM](https://nodei.co/npm/yeps-virtual-host.png)](https://npmjs.org/package/yeps-virtual-host)
+
+[![npm version](https://badge.fury.io/js/yeps-virtual-host.svg)](https://badge.fury.io/js/yeps-virtual-host)
+[![Build Status](https://travis-ci.org/evheniy/yeps-virtual-host.svg?branch=master)](https://travis-ci.org/evheniy/yeps-virtual-host)
+[![Coverage Status](https://coveralls.io/repos/github/evheniy/yeps-virtual-host/badge.svg?branch=master)](https://coveralls.io/github/evheniy/yeps-virtual-host?branch=master)
+[![Linux Build](https://img.shields.io/travis/evheniy/yeps-virtual-host/master.svg?label=linux)](https://travis-ci.org/evheniy/)
+[![Windows Build](https://img.shields.io/appveyor/ci/evheniy/yeps-virtual-host/master.svg?label=windows)](https://ci.appveyor.com/project/evheniy/yeps-virtual-host)
+
+[![Dependency Status](https://david-dm.org/evheniy/yeps-virtual-host.svg)](https://david-dm.org/evheniy/yeps-virtual-host)
+[![devDependency Status](https://david-dm.org/evheniy/yeps-virtual-host/dev-status.svg)](https://david-dm.org/evheniy/yeps-virtual-host#info=devDependencies)
+[![NSP Status](https://img.shields.io/badge/NSP%20status-no%20vulnerabilities-green.svg)](https://travis-ci.org/evheniy/yeps-virtual-host)
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/evheniy/yeps-virtual-host/master/LICENSE)
+[![GitHub stars](https://img.shields.io/github/stars/evheniy/yeps-virtual-host.svg)](https://github.com/evheniy/yeps-virtual-host/stargazers)
+[![GitHub forks](https://img.shields.io/github/forks/evheniy/yeps-virtual-host.svg)](https://github.com/evheniy/yeps-virtual-host/network)
+[![GitHub issues](https://img.shields.io/github/issues/evheniy/yeps-virtual-host.svg)](https://github.com/evheniy/yeps-virtual-host/issues)
+[![Twitter](https://img.shields.io/twitter/url/https/github.com/evheniy/yeps-virtual-host.svg?style=social)](https://twitter.com/intent/tweet?text=Wow:&url=%5Bobject%20Object%5D)
+
+
+## How to install
+
+    npm i -S yeps-virtual-host
+  
+
+## How to use
+
+### YEPS config:
+
+    const App = require('yeps');
+    const VirtualHost = require('yeps-virtual-host');
+    const error = require('yeps-error');
+    
+    const app = new App();
+    const vhost = new VirtualHost();
+    
+    app.all([
+        error();
+    ]);
+    
+### HTTP:
+
+    vhost
+        .http('yeps.info')
+        .then(async ctx => {
+            ctx.res.statusCode = 200;
+            ctx.res.end('homepage');     
+        });
+        
+### HTTPS:
+
+    vhost
+        .https('yeps.info')
+        .then(async ctx => {
+            ctx.res.statusCode = 302;
+            ctx.res.setHeader('Location', 'http://yeps.info/');
+            ctx.res.end();     
+        });
+        
+### Any host:
+
+    vhost
+        .any('*.yeps.info')
+        .then(async ctx => {
+            ctx.res.statusCode = 302;
+            ctx.res.setHeader('Location', 'http://yeps.info/');
+            ctx.res.end();      
+        });    
+            
+### All methods are wrappers for catch() method:
+
+    catch({ 
+        domain, 
+        protocol: '*|http|https' 
+    })
+
+Example: 
+
+    vhost
+        .catch({ domain: '*.yeps.info', protocol: '*' })
+        .then(async ctx => {
+            ctx.res.statusCode = 302;
+            ctx.res.setHeader('Location', 'http://yeps.info/');
+            ctx.res.end();      
+        });
+    
+### Add virtual host to app:
+    
+    app.then(vhost.resolve());
+
+
+### With router:
+
+    const Router = require('yeps-router');
+    
+    const siteRouter = new Router();
+    
+    siteRouter.get('/').then(async ctx => {
+        ctx.res.statusCode = 200;
+        ctx.res.end('homepage');     
+    });
+    
+    vhost
+        .http('yeps.info')
+        .then(siteRouter.resolve());    
+
+
+    const apiRouter = new Router();
+    
+    apiRouter.get('/').then(async ctx => {
+        ctx.res.statusCode = 200;
+        ctx.res.setHeader('Content-Type', 'application/json');
+        ctx.res.end('{"status":"OK"}');     
+    });
+    
+    vhost
+        .http('api.yeps.info')
+        .then(apiRouter.resolve()); 
+    
+    
+    app.then(vhost.resolve());
+
+#### [YEPS documentation](http://yeps.info/)
---
+++ tests/index.js
@@ -0,0 +1,244 @@
+const App = require('yeps');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { resolve } = require('path');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const error = require('yeps-error');
+const VirtualHost = require('..');
+const Router = require('yeps-router');
+const expect = chai.expect;
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const options = {
+    key: fs.readFileSync(resolve(__dirname, 'ssl', 'key.pem')),
+    cert: fs.readFileSync(resolve(__dirname, 'ssl', 'cert.pem'))
+};
+
+chai.use(chaiHttp);
+let app;
+
+describe('YEPS cookies parser', async () => {
+
+    beforeEach(() => {
+        app = new App();
+        app.then(error());
+    });
+
+    it('should test http method', async () => {
+        let isTestFinished1 = false;
+        let isTestFinished2 = false;
+
+        const vhost = new VirtualHost();
+
+        vhost.http('yeps.info').then(async ctx => {
+            isTestFinished1 = true;
+            ctx.res.statusCode = 200;
+            ctx.res.end('');
+        });
+
+        app.then(vhost.resolve());
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .set('Host', 'yeps.info')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                isTestFinished2 = true;
+            });
+
+        expect(isTestFinished1).is.true;
+        expect(isTestFinished2).is.true;
+    });
+
+    it('should test all method for http', async () => {
+        let isTestFinished1 = false;
+        let isTestFinished2 = false;
+
+        const vhost = new VirtualHost();
+
+        vhost.all('yeps.info').then(async ctx => {
+            isTestFinished1 = true;
+            ctx.res.statusCode = 200;
+            ctx.res.end('');
+        });
+
+        app.then(vhost.resolve());
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .set('Host', 'yeps.info')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                isTestFinished2 = true;
+            });
+
+        expect(isTestFinished1).is.true;
+        expect(isTestFinished2).is.true;
+    });
+
+    it('should test https method', async () => {
+        let isTestFinished1 = false;
+        let isTestFinished2 = false;
+
+        const vhost = new VirtualHost();
+
+        vhost.https('yeps.info').then(async ctx => {
+            isTestFinished1 = true;
+            ctx.res.statusCode = 200;
+            ctx.res.end('');
+        });
+
+        app.then(vhost.resolve());
+
+        await chai.request(https.createServer(options, app.resolve()))
+            .get('/')
+            .set('Host', 'yeps.info')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                isTestFinished2 = true;
+            });
+
+        expect(isTestFinished1).is.true;
+        expect(isTestFinished2).is.true;
+    });
+
+    it('should test all method for https', async () => {
+        let isTestFinished1 = false;
+        let isTestFinished2 = false;
+
+        const vhost = new VirtualHost();
+
+        vhost.all('yeps.info').then(async ctx => {
+            isTestFinished1 = true;
+            ctx.res.statusCode = 200;
+            ctx.res.end('');
+        });
+
+        app.then(vhost.resolve());
+
+        await chai.request(https.createServer(options, app.resolve()))
+            .get('/')
+            .set('Host', 'yeps.info')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                isTestFinished2 = true;
+            });
+
+        expect(isTestFinished1).is.true;
+        expect(isTestFinished2).is.true;
+    });
+
+    it('should test 404 error', async () => {
+        let isTestFinished = false;
+
+        const vhost = new VirtualHost();
+
+        vhost.http('api.yeps.info').then(async ctx => {
+            ctx.res.statusCode = 200;
+            ctx.res.end('');
+        });
+
+        app.then(vhost.resolve());
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .set('Host', 'yeps.info')
+            .send()
+            .catch(err => {
+                expect(err).to.have.status(404);
+                isTestFinished = true;
+            });
+
+        expect(isTestFinished).is.true;
+    });
+
+    it('should test 500 error', async () => {
+        let isTestFinished = false;
+
+        const vhost = new VirtualHost();
+
+        vhost.http('yeps.info').then(async () => {
+            throw new Error('test');
+        });
+
+        app.then(vhost.resolve());
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .set('Host', 'yeps.info')
+            .send()
+            .catch(err => {
+                expect(err).to.have.status(500);
+                isTestFinished = true;
+            });
+
+        expect(isTestFinished).is.true;
+    });
+
+    it('should test router', async () => {
+        let isTestFinished1 = false;
+        let isTestFinished2 = false;
+
+        const router = new Router();
+
+        router.get('/').then(async ctx => {
+            isTestFinished1 = true;
+            ctx.res.statusCode = 200;
+            ctx.res.end('homepage');
+        });
+
+        const vhost = new VirtualHost();
+
+        vhost
+            .http('yeps.info')
+            .then(router.resolve());
+
+        app.then(vhost.resolve());
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .set('Host', 'yeps.info')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                isTestFinished2 = true;
+            });
+
+        expect(isTestFinished1).is.true;
+        expect(isTestFinished2).is.true;
+    });
+
+    it('should test regexp domain', async () => {
+        let isTestFinished1 = false;
+        let isTestFinished2 = false;
+
+        const vhost = new VirtualHost();
+
+        vhost.http('*.yeps.info').then(async ctx => {
+            isTestFinished1 = true;
+            ctx.res.statusCode = 200;
+            ctx.res.end('');
+        });
+
+        app.then(vhost.resolve());
+
+        await chai.request(http.createServer(app.resolve()))
+            .get('/')
+            .set('Host', 'api.yeps.info')
+            .send()
+            .then(res => {
+                expect(res).to.have.status(200);
+                isTestFinished2 = true;
+            });
+
+        expect(isTestFinished1).is.true;
+        expect(isTestFinished2).is.true;
+    });
+
+});
