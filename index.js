777966d3f4c3337a7939cfe0156e24b6cef0c357
Evheniy Bystrov <evheniy.bystrov@gmail.com>
2017-10-29 09:39:14 +0200

    updates

--- index.js
+++ index.js
@@ -6,86 +6,81 @@ const ESCAPE_REGEXP = /([.+?^=!:${}()|[\]/\\])/g;
 const ESCAPE_REPLACE = '\\$1';
 
 const isRegexp = val => Object.prototype.toString.call(val) === '[object RegExp]';
-const getRegexp = val => {
-    let source = !isRegexp(val)
-        ? String(val)
-            .replace(ESCAPE_REGEXP, ESCAPE_REPLACE)
-            .replace(ASTERISK_REGEXP, ASTERISK_REPLACE)
-        : val.source;
+const getRegexp = (val) => {
+  const source = !isRegexp(val)
+    ? String(val)
+      .replace(ESCAPE_REGEXP, ESCAPE_REPLACE)
+      .replace(ASTERISK_REGEXP, ASTERISK_REPLACE)
+    : val.source;
 
-    return new RegExp(`^${source}$`, 'i');
+  return new RegExp(`^${source}$`, 'i');
 };
 
 module.exports = class {
+  constructor() {
+    debug('VirtualHost created!');
 
-    constructor() {
-        debug('VirtualHost created!');
+    this.domains = [];
+    this.protocols = [
+      'http',
+      'https',
+    ];
 
-        this.domains = [];
-        this.protocols = [
-            'http',
-            'https',
-        ];
+    this.protocols.forEach((protocol) => {
+      this[protocol.toLowerCase()] = (domain) => {
+        debug('%s %s', protocol, domain);
+        return this.catch({ domain, protocol });
+      };
+    });
+  }
 
-        this.protocols.forEach(protocol => {
-            this[protocol.toLowerCase()] = domain => {
-                debug('%s %s', protocol, domain);
-                return this.catch({ domain, protocol });
-            };
+  all(domain) {
+    debug('* %s', domain);
+
+    return this.catch({ domain, protocol: '*' });
+  }
+
+  resolve() {
+    debug('VirtualHost started!');
+
+    return ctx => Promise.all(this.domains.map(domain => domain(ctx)))
+      .then(() => debug('Host not found!'))
+      .catch(async (fn) => {
+        await fn(ctx);
+        return Promise.reject();
+      });
+  }
+
+  catch({ domain = /.*/, protocol = '*' } = {}) {
+    const { domains } = this;
+    const that = this;
+
+    return {
+      then(fn) {
+        domains.push(async (ctx) => {
+          const requestProtocol = ctx.req.connection.encrypted ? 'https' : 'http';
+          debug('requestProtocol: %s', requestProtocol);
+          const hostname = ctx.req.headers.host || '';
+          debug('hostname: %s', hostname);
+          const regexp = getRegexp(domain);
+          debug('regexp: %s', regexp);
+          const isProtocolMatched = (protocol === '*' || protocol === requestProtocol);
+          debug('isProtocolMatched: %s', isProtocolMatched);
+          const isDomainMatched = !!regexp.exec(hostname);
+          debug(regexp.exec(domain));
+          debug('isDomainMatched: %s', isDomainMatched);
+
+          if (isProtocolMatched && isDomainMatched) {
+            debug('Domain found!');
+            debug('%s %s', domain, requestProtocol);
+
+            return Promise.reject(fn);
+          }
+          return Promise.resolve(ctx);
         });
-    }
 
-    all(domain) {
-        debug('* %s', domain);
-
-        return this.catch({ domain, protocol: '*' });
-    }
-
-    resolve() {
-        debug('VirtualHost started!');
-
-        return ctx => Promise.all(this.domains.map(domain => domain(ctx)))
-            .then(() => debug('Host not found!'))
-            .catch(async fn => {
-                await fn(ctx);
-                return Promise.reject();
-            });
-    }
-
-    catch({ domain = /.*/, protocol = '*' } = {}) {
-        const domains = this.domains;
-        const that = this;
-
-        return {
-            then(fn) {
-                domains.push(async ctx => {
-
-                    const requestProtocol = ctx.req.connection.encrypted ? 'https' : 'http';
-                    debug('requestProtocol: %s', requestProtocol);
-                    const hostname = ctx.req.headers.host || '';
-                    debug('hostname: %s', hostname);
-                    const regexp = getRegexp(domain);
-                    debug('regexp: %s', regexp);
-                    const isProtocolMatched = (protocol === '*' || protocol === requestProtocol);
-                    debug('isProtocolMatched: %s', isProtocolMatched);
-                    const isDomainMatched = !!regexp.exec(hostname);
-                    debug(regexp.exec(domain));
-                    debug('isDomainMatched: %s', isDomainMatched);
-
-                    if (isProtocolMatched && isDomainMatched) {
-
-                        debug('Domain found!');
-                        debug('%s %s', domain, requestProtocol);
-
-                        return Promise.reject(fn);
-                    } else {
-                        return Promise.resolve(ctx);
-                    }
-                });
-
-                return that;
-            }
-        };
-    }
-
+        return that;
+      },
+    };
+  }
 };
--- tests/index.js
+++ tests/index.js
@@ -1,298 +1,313 @@
 const App = require('yeps');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { resolve } = require('path');
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
 const error = require('yeps-error');
 const VirtualHost = require('..');
 const Router = require('yeps-router');
-const expect = chai.expect;
+const srv = require('yeps-server');
+const pem = require('pem');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-const options = {
-    key: fs.readFileSync(resolve(__dirname, 'ssl', 'key.pem')),
-    cert: fs.readFileSync(resolve(__dirname, 'ssl', 'cert.pem'))
-};
+const { expect } = chai;
 
 chai.use(chaiHttp);
 let app;
+let server;
 
 describe('YEPS cookies parser', async () => {
+  beforeEach(() => {
+    app = new App();
+    app.then(error());
+    server = srv.createHttpServer(app);
+  });
 
-    beforeEach(() => {
-        app = new App();
-        app.then(error());
+  afterEach(() => {
+    server.close();
+  });
+
+  it('should test http method', async () => {
+    let isTestFinished1 = false;
+    let isTestFinished2 = false;
+
+    const vhost = new VirtualHost();
+
+    vhost.http('yeps.info').then(async (ctx) => {
+      isTestFinished1 = true;
+      ctx.res.statusCode = 200;
+      ctx.res.end('');
     });
 
-    it('should test http method', async () => {
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
+    app.then(vhost.resolve());
 
-        const vhost = new VirtualHost();
+    await chai.request(server)
+      .get('/')
+      .set('Host', 'yeps.info')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished2 = true;
+      });
 
-        vhost.http('yeps.info').then(async ctx => {
-            isTestFinished1 = true;
-            ctx.res.statusCode = 200;
-            ctx.res.end('');
+    expect(isTestFinished1).is.true;
+    expect(isTestFinished2).is.true;
+  });
+
+  it('should test all method for http', async () => {
+    let isTestFinished1 = false;
+    let isTestFinished2 = false;
+
+    const vhost = new VirtualHost();
+
+    vhost.all('yeps.info').then(async (ctx) => {
+      isTestFinished1 = true;
+      ctx.res.statusCode = 200;
+      ctx.res.end('');
+    });
+
+    app.then(vhost.resolve());
+
+    await chai.request(server)
+      .get('/')
+      .set('Host', 'yeps.info')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished2 = true;
+      });
+
+    expect(isTestFinished1).is.true;
+    expect(isTestFinished2).is.true;
+  });
+
+  it('should test https method', async () => {
+    let isTestFinished = false;
+
+    const vhost = new VirtualHost();
+
+    vhost.https('yeps.info').then(async (ctx) => {
+      isTestFinished = true;
+      ctx.res.statusCode = 200;
+      ctx.res.end('');
+    });
+
+    app.then(vhost.resolve());
+
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+    const days = 1;
+    const selfSigned = true;
+
+    pem.createCertificate({ days, selfSigned }, (err, { serviceKey: key, certificate: cert }) => {
+      if (err) {
+        throw err;
+      }
+      const httpsServer = srv.createHttpsServer({ key, cert }, app);
+
+      chai.request(httpsServer)
+        .get('/')
+        .set('Host', 'yeps.info')
+        .send()
+        .end((e, res) => {
+          expect(e).to.be.null;
+          expect(res).to.have.status(200);
+          expect(isTestFinished).is.true;
         });
+    });
+  });
 
-        app.then(vhost.resolve());
+  it('should test all method for https', async () => {
+    let isTestFinished = false;
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .set('Host', 'yeps.info')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished2 = true;
-            });
+    const vhost = new VirtualHost();
 
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
+    vhost.all('yeps.info').then(async (ctx) => {
+      isTestFinished = true;
+      ctx.res.statusCode = 200;
+      ctx.res.end('');
     });
 
-    it('should test all method for http', async () => {
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
+    app.then(vhost.resolve());
 
-        const vhost = new VirtualHost();
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-        vhost.all('yeps.info').then(async ctx => {
-            isTestFinished1 = true;
-            ctx.res.statusCode = 200;
-            ctx.res.end('');
+    const days = 1;
+    const selfSigned = true;
+
+    pem.createCertificate({ days, selfSigned }, (err, { serviceKey: key, certificate: cert }) => {
+      if (err) {
+        throw err;
+      }
+      const httpsServer = srv.createHttpsServer({ key, cert }, app);
+
+      chai.request(httpsServer)
+        .get('/')
+        .set('Host', 'yeps.info')
+        .send()
+        .end((e, res) => {
+          expect(e).to.be.null;
+          expect(res).to.have.status(200);
+          expect(isTestFinished).is.true;
         });
+    });
+  });
 
-        app.then(vhost.resolve());
+  it('should test 404 error', async () => {
+    let isTestFinished = false;
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .set('Host', 'yeps.info')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished2 = true;
-            });
+    const vhost = new VirtualHost();
 
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
+    vhost.http('api.yeps.info').then(async (ctx) => {
+      ctx.res.statusCode = 200;
+      ctx.res.end('');
     });
 
-    it('should test https method', async () => {
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
+    app.then(vhost.resolve());
 
-        const vhost = new VirtualHost();
+    await chai.request(server)
+      .get('/')
+      .set('Host', 'yeps.info')
+      .send()
+      .catch((err) => {
+        expect(err).to.have.status(404);
+        isTestFinished = true;
+      });
 
-        vhost.https('yeps.info').then(async ctx => {
-            isTestFinished1 = true;
-            ctx.res.statusCode = 200;
-            ctx.res.end('');
-        });
+    expect(isTestFinished).is.true;
+  });
 
-        app.then(vhost.resolve());
+  it('should test 500 error', async () => {
+    let isTestFinished = false;
 
-        await chai.request(https.createServer(options, app.resolve()))
-            .get('/')
-            .set('Host', 'yeps.info')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished2 = true;
-            });
+    const vhost = new VirtualHost();
 
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
+    vhost.http('yeps.info').then(async () => {
+      throw new Error('test');
     });
 
-    it('should test all method for https', async () => {
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
+    app.then(vhost.resolve());
 
-        const vhost = new VirtualHost();
+    await chai.request(server)
+      .get('/')
+      .set('Host', 'yeps.info')
+      .send()
+      .catch((err) => {
+        expect(err).to.have.status(500);
+        isTestFinished = true;
+      });
 
-        vhost.all('yeps.info').then(async ctx => {
-            isTestFinished1 = true;
-            ctx.res.statusCode = 200;
-            ctx.res.end('');
-        });
+    expect(isTestFinished).is.true;
+  });
 
-        app.then(vhost.resolve());
+  it('should test router', async () => {
+    let isTestFinished1 = false;
+    let isTestFinished2 = false;
 
-        await chai.request(https.createServer(options, app.resolve()))
-            .get('/')
-            .set('Host', 'yeps.info')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished2 = true;
-            });
+    const router = new Router();
 
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
+    router.get('/').then(async (ctx) => {
+      isTestFinished1 = true;
+      ctx.res.statusCode = 200;
+      ctx.res.end('homepage');
     });
 
-    it('should test 404 error', async () => {
-        let isTestFinished = false;
+    const vhost = new VirtualHost();
 
-        const vhost = new VirtualHost();
+    vhost
+      .http('yeps.info')
+      .then(router.resolve());
 
-        vhost.http('api.yeps.info').then(async ctx => {
-            ctx.res.statusCode = 200;
-            ctx.res.end('');
-        });
+    app.then(vhost.resolve());
 
-        app.then(vhost.resolve());
+    await chai.request(server)
+      .get('/')
+      .set('Host', 'yeps.info')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished2 = true;
+      });
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .set('Host', 'yeps.info')
-            .send()
-            .catch(err => {
-                expect(err).to.have.status(404);
-                isTestFinished = true;
-            });
+    expect(isTestFinished1).is.true;
+    expect(isTestFinished2).is.true;
+  });
 
-        expect(isTestFinished).is.true;
+  it('should test regexp string  domain', async () => {
+    let isTestFinished1 = false;
+    let isTestFinished2 = false;
+
+    const vhost = new VirtualHost();
+
+    vhost.http('*.yeps.info').then(async (ctx) => {
+      isTestFinished1 = true;
+      ctx.res.statusCode = 200;
+      ctx.res.end('');
     });
 
-    it('should test 500 error', async () => {
-        let isTestFinished = false;
+    app.then(vhost.resolve());
 
-        const vhost = new VirtualHost();
+    await chai.request(server)
+      .get('/')
+      .set('Host', 'api.yeps.info')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished2 = true;
+      });
 
-        vhost.http('yeps.info').then(async () => {
-            throw new Error('test');
-        });
+    expect(isTestFinished1).is.true;
+    expect(isTestFinished2).is.true;
+  });
 
-        app.then(vhost.resolve());
+  it('should test regexp domain', async () => {
+    let isTestFinished1 = false;
+    let isTestFinished2 = false;
 
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .set('Host', 'yeps.info')
-            .send()
-            .catch(err => {
-                expect(err).to.have.status(500);
-                isTestFinished = true;
-            });
+    const vhost = new VirtualHost();
 
-        expect(isTestFinished).is.true;
+    vhost.http(/yeps.info/).then(async (ctx) => {
+      isTestFinished1 = true;
+      ctx.res.statusCode = 200;
+      ctx.res.end('');
     });
 
-    it('should test router', async () => {
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
+    app.then(vhost.resolve());
 
-        const router = new Router();
+    await chai.request(server)
+      .get('/')
+      .set('Host', 'yeps.info')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished2 = true;
+      });
 
-        router.get('/').then(async ctx => {
-            isTestFinished1 = true;
-            ctx.res.statusCode = 200;
-            ctx.res.end('homepage');
-        });
+    expect(isTestFinished1).is.true;
+    expect(isTestFinished2).is.true;
+  });
 
-        const vhost = new VirtualHost();
+  it('should test empty catch', async () => {
+    let isTestFinished1 = false;
+    let isTestFinished2 = false;
 
-        vhost
-            .http('yeps.info')
-            .then(router.resolve());
+    const vhost = new VirtualHost();
 
-        app.then(vhost.resolve());
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .set('Host', 'yeps.info')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished2 = true;
-            });
-
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
+    vhost.catch().then(async (ctx) => {
+      isTestFinished1 = true;
+      ctx.res.statusCode = 200;
+      ctx.res.end('');
     });
 
-    it('should test regexp string  domain', async () => {
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
+    app.then(vhost.resolve());
 
-        const vhost = new VirtualHost();
+    await chai.request(server)
+      .get('/')
+      .set('Host', '')
+      .send()
+      .then((res) => {
+        expect(res).to.have.status(200);
+        isTestFinished2 = true;
+      });
 
-        vhost.http('*.yeps.info').then(async ctx => {
-            isTestFinished1 = true;
-            ctx.res.statusCode = 200;
-            ctx.res.end('');
-        });
-
-        app.then(vhost.resolve());
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .set('Host', 'api.yeps.info')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished2 = true;
-            });
-
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
-    });
-
-    it('should test regexp domain', async () => {
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
-
-        const vhost = new VirtualHost();
-
-        vhost.http(/yeps.info/).then(async ctx => {
-            isTestFinished1 = true;
-            ctx.res.statusCode = 200;
-            ctx.res.end('');
-        });
-
-        app.then(vhost.resolve());
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .set('Host', 'yeps.info')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished2 = true;
-            });
-
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
-    });
-
-    it('should test empty catch', async () => {
-        let isTestFinished1 = false;
-        let isTestFinished2 = false;
-
-        const vhost = new VirtualHost();
-
-        vhost.catch().then(async ctx => {
-            isTestFinished1 = true;
-            ctx.res.statusCode = 200;
-            ctx.res.end('');
-        });
-
-        app.then(vhost.resolve());
-
-        await chai.request(http.createServer(app.resolve()))
-            .get('/')
-            .set('Host', '')
-            .send()
-            .then(res => {
-                expect(res).to.have.status(200);
-                isTestFinished2 = true;
-            });
-
-        expect(isTestFinished1).is.true;
-        expect(isTestFinished2).is.true;
-    });
-
+    expect(isTestFinished1).is.true;
+    expect(isTestFinished2).is.true;
+  });
 });
